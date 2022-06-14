4c4075dd5b429ad8726892921359fdcb15bb14d8
agustina grandon <agustina107.grandon@gmail.com>
2022-06-14 17:44:01 -0300

    agrege html y css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="titulo">//  TITULO  //</h1>
+        <h1 className="titulo">TITULO</h1>
 
         <ul className="nav justify-content-center">
           <li className="nav-item">
