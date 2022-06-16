7fcf4f9d7efafecf512d04100252301e678a494b
agustina grandon <agustina107.grandon@gmail.com>
2022-06-16 18:02:44 -0300

    agrege componentes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
+//@ts-check
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="titulo">TITULO</h1>
-
-        <ul className="nav justify-content-center">
-          <li className="nav-item">
-            <a className="nav-link" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Seccion 1</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Seccion 2</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Seccion 3</a>
-          </li>
-        </ul>
-      </header>
+       
     </div>
   );
 }
