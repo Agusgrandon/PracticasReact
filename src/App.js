5a75827cb4cfb1db8f94b268f9e36688929bb9ef
agustina grandon <agustina107.grandon@gmail.com>
2022-06-20 15:25:58 -0300

    agrege el titulo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 //@ts-check
 import './App.css';
-
+import Titulo from './Components/Titulo';
 
 
 function App() {
   return (
     <div className="App">
-       
+       <header>
+       <Titulo></Titulo>
+       </header>
     </div>
   );
 }
---
+++ src/Components/Titulo.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import './Titulo.css';
+
+class Titulo extends Component{
+  render(){
+    return <h1 className="titulo">PLAYADITO</h1>
+  }
+
+}
+ export default Titulo;
